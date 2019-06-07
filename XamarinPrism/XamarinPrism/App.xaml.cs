583b0906231d545befc12734f5108ee9308197fc
Robson Correia <robsoncorreia01@outlook.com>
2019-06-07 07:51:29 -0300

    Add project files.

---
+++ XamarinPrism/XamarinPrism/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Prism;
+using Prism.Ioc;
+using XamarinPrism.ViewModels;
+using XamarinPrism.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinPrism.Views.Login;
+using XamarinPrism.ViewModels.Login;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XamarinPrism
+{
+    public partial class App
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/LoginPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<LoginPage, LoginViewModel>();
+        }
+    }
+}
---
+++ XamarinPrism/XamarinPrism/ViewModels/Login/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Navigation;
+
+namespace XamarinPrism.ViewModels.Login
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public LoginViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Login";
+        }
+    }
+}
